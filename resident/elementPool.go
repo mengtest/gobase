b6de28d2e03be00a2a26dd0c95275cca98590d0c
Administrator <administrator@ubuntu.ubuntu-domain>
2018-06-02 03:41:26 +0800

    增加说明

--- resident/elementPool.go
+++ resident/elementPool.go
@@ -1,1 +1,49 @@
+/**************************************************************************************
+Code Description    : 元素池
+Code Vesion         :
+					|------------------------------------------------------------|
+						  Version    					Editor            Time
+							1.0        					yuansudong        2016.4.12
+					|------------------------------------------------------------|
+Version Description	:
+                    |------------------------------------------------------------|
+						  Version
+							1.0
+								 ....
+					|------------------------------------------------------------|
+***************************************************************************************/
+
 package resident
+
+import (
+	"sync"
+)
+
+var (
+	elePool *elementPool
+)
+
+func init() {
+	elePool = newElementPool()
+}
+
+type elementPool struct {
+	pool sync.Pool
+}
+
+func newElementPool() *elementPool {
+	return &elementPool{
+		pool: sync.Pool{
+			New: func() interface{} {
+				return newElement()
+			},
+		},
+	}
+}
+
+func (ep *elementPool) get() *element {
+	return ep.pool.Get().(*element)
+}
+func (ep *elementPool) put(ele *element) {
+	ep.pool.Put(ele)
+}
