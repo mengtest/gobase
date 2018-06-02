ca904b1be74763df05b1438b8556c234c49007fc
Administrator <administrator@ubuntu.ubuntu-domain>
2018-06-02 23:48:41 +0800

    对象管理者

--- pool/manager.go
+++ pool/manager.go
@@ -6,11 +6,51 @@ import (
 
 // Manager 用于管理池
 type Manager struct {
-	value int
-	pools []map[string]sync.Pool
+	lock  sync.Mutex
+	pools map[string]sync.Pool
 }
 
 // NewManager 用于新建一个Manager
 func NewManager() *Manager {
+	pManager := &Manager{
+		pools: make(map[string]sync.Pool),
+	}
+	return pManager
+}
 
+// Register 用于注册新的对象池
+func (m *Manager) Register(name string,
+	create func() interface{}) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	m.pools[name] = sync.Pool{
+		New: create,
+	}
+}
+
+// UnRegister 用于注销已有的对象池
+func (m *Manager) UnRegister(name string) {
+	m.lock.Lock()
+	m.lock.Unlock()
+	delete(m.pools, name)
+}
+
+// Get 用于获取指定模块的对象
+func (m *Manager) Get(name string) interface{} {
+	m.lock.Lock()
+	m.lock.Unlock()
+	model, ok := m.pools[name]
+	if ok {
+		return model.Get()
+	}
+	return nil
+}
+
+// Put 用于将对象放回到池子里
+func (m *Manager) Put(name string, data interface{}) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	if model, ok := m.pools[name]; ok {
+		model.Put(data)
+	}
 }
