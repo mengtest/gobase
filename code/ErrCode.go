76ccf1002712d7227c3bc6304d447926f625d5e0
1145919989@qq.com <1145919989@qq.com>
2018-08-17 01:55:06 +0800

    加

---
+++ code/ErrCode.go
@@ -0,0 +1,23 @@
+package code
+
+const (
+	// ErrFailedCode 看英文
+	ErrFailedCode int = -1
+	// ErrSuccessCode 看英文
+	ErrSuccessCode int = 0
+	// ErrBusyCode 看英文
+	ErrBusyCode int = 1
+	// ErrArgCode 参数错误
+	ErrArgCode int = 2
+)
+
+const (
+	// ErrFailedDesc 看英文
+	ErrFailedDesc string = "失败"
+	// ErrSuccessDesc 看英文
+	ErrSuccessDesc string = "成功"
+	// ErrBusyDesc 看英文
+	ErrBusyDesc string = "系统繁忙,请稍后重试"
+	// ErrArgDesc ..
+	ErrArgDesc string = "参数错误"
+)
